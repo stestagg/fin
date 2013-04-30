2f480051e908f4bcf75e748c2be24ca35565e87c
Steve Stagg <stestagg@gmail.com>
2013-04-30 12:04:11 +0100

    Update version to 1.31

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     setup(
         name="fin",
-        version="1.3",
+        version="1.31",
         license="BSD",
 
         description="A small, useful python utility library",
